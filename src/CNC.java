cb6e1aafc8c720ed92698cb60946aa10ce9cbde3
StrayBird_ATSH <zhanghuilian-98022@163.com>
2018-09-15 12:15:10 +0800

    Revised version for the second case.

--- src/CNC.java
+++ src/CNC.java
@@ -1,5 +1,10 @@
 class CNC {
-    WorkingType_CNC workingType = WorkingType_CNC.WAITING;
+    WorkingType_CNC workingType;
     int timeStarted = 0;
-    int blade = 0;
+    int blade;
+
+    CNC(int blade) {
+        this.blade = blade;
+        this.workingType = blade==0?WorkingType_CNC.WAITINGA:WorkingType_CNC.WAITINGB;
+    }
 }
